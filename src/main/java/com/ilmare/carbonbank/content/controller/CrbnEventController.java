63a6c3419cefa96483ddb5886e274e71ffff8717
drag1 <drag1@192.168.219.104>
2025-05-31 00:47:52 +0900

    application.yml에 저장경로 적용 필요

--- src/main/java/com/ilmare/carbonbank/content/controller/CrbnEventController.java
+++ src/main/java/com/ilmare/carbonbank/content/controller/CrbnEventController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -58,14 +59,13 @@ public class CrbnEventController {
 	public String EventMainList(HttpServletRequest request, final NewsCommonModel paramVo, Model model) throws Exception {
 		
 		log.info("EventMainList Start");
+		
 		sessMgr.createSession(request, false);
 		if ( !sessMgr.isSession() ) {
-//		if ( !sessMgr.isSession(request) ) {
 			log.info("EventMainList 세션 없음 상태");
 			return "redirect:" + conConst.lgnUrl;
 		}
 		
-//		SessInfo sessInfo = sessMgr.getSession(request);
 		SessInfo sessInfo = sessMgr.getSessInfo();
 		log.info("EventMainList 로그인 상태");
 		log.info("EventMainList sessInfo=" + sessInfo.toString());
@@ -75,26 +75,10 @@ public class CrbnEventController {
 		if ( !commSvc.checkContentUse(sessInfo.getPartyGrp()) ) {
 			log.info("EventMainList 권한 없음 상태");
 			return "redirect:" + conConst.lgnUrl;
-		}
+		}		
 		
-		//메뉴 조회
-		//List menuList = iUserInfoService.getMenu(userInfoVO);
-		
-		//이벤트 리스트 조회
-        int pageSize = conConst.pageSize;    //페이지당 row 건수
-        int pageNo = paramVo.getPageNo(); //조회할 페이지 번호
-        int sRowNum = ((pageNo - 1) * pageSize) ;    //조회할 row의 시작값
-		log.info("EventMainList {} ~ {}", sRowNum, pageSize);
-		paramVo.setPageNo(sRowNum);
-		paramVo.setListSize(pageSize);
-		
-		List<NewsCommonModel> ntsList = svc.selectAdmList(paramVo);
-		log.info("EventMainList ntsList.size()" + ntsList.toString());
-		
+		model.addAttribute("sessInfo", sessInfo);		
 
-		model.addAttribute("sessInfo", sessInfo);
-		model.addAttribute("ntsList", ntsList);
-		//model.addAttribute("menuList", menuList);
 		log.info("EventMainList End");
 
 		return "adm/content/event/list";
@@ -105,7 +89,12 @@ public class CrbnEventController {
 	 * 버튼 클릭조회
 	 */
 	@RequestMapping("/EventQueryList")
-	public  @ResponseBody HashMap EventQueryList(HttpServletRequest request, final NewsCommonModel paramVo, Model model) throws Exception {
+	public  @ResponseBody HashMap EventQueryList(
+			HttpServletRequest request, 
+			final NewsCommonModel paramVo, 
+			Model model,			
+		    @RequestParam(defaultValue = "1") int page
+			) throws Exception {
 		
 		HashMap result = new HashMap();
 		log.info("NoticeQueryList Start");
@@ -137,12 +126,28 @@ public class CrbnEventController {
         int pageNo = paramVo.getPageNo(); //조회할 페이지 번호
         int sRowNum = ((pageNo - 1) * pageSize) ;    //조회할 row의 시작값
 		log.info("EventMainList {} ~ {}", sRowNum, pageSize);
+		
 		paramVo.setPageNo(sRowNum);
 		paramVo.setListSize(pageSize);
 		paramVo.setListSize(ConfigConstants.pageSize);
-		List<NewsCommonModel> ntsList = svc.selectAdmList(paramVo);
+		
+		List<NewsCommonModel> dataList = svc.selectAdmList(paramVo);
 
-		result.put("ntsList", ntsList);
+
+		///////////////////////////////
+		String totalCountStr = svc.selectAdmListCount(paramVo);
+
+	    int totalCount = 0;
+	    try { totalCount = Integer.parseInt(totalCountStr); } catch(NumberFormatException e) { totalCount = 0; }
+	    
+	    int totalPages = (int) Math.ceil((double) totalCount / pageSize);
+
+	    result.put("currentPage", pageNo);  // 타임리프에 돌려줄 페이지번호
+	    result.put("totalCount", totalCountStr);
+	    result.put("totalPages", totalPages);
+		///////////////////////////////
+		result.put("rows", dataList); // jquery는 이 데이터만 사용.
+		
 		log.info("NoticeQueryList End");
 
 		return result;
