2a157df8816ed0405129a8ebbb667d139094d586
drag1 <drag1@192.168.219.104>
2025-06-03 01:30:57 +0900

    파일 처리 삭제

--- src/main/java/com/ilmare/carbonbank/content/controller/CrbnFaqController.java
+++ src/main/java/com/ilmare/carbonbank/content/controller/CrbnFaqController.java
@@ -308,41 +308,7 @@ public class CrbnFaqController {
 			return result;
 		}
 		
-		//파일 관련
-		if ( imgFile != null && !imgFile.isEmpty()) {
-			String fileSavePath = FileUtil.getSaveFilePath("pcTemp", DateUtil.getCurrDate());
-			log.info("파일 이름: " + imgFile.getOriginalFilename());
-			log.info("fileSavePath: {} " ,fileSavePath);
-
-	        String fileExt = imgFile.getOriginalFilename().substring(imgFile.getOriginalFilename().lastIndexOf("."));
-			String originalFilename = imgFile.getOriginalFilename();
-			//String imgNailNm = fileSavePath +File.separator + "640"+DateUtil.getCurrDateTime()+"." + fileExt;
-			String imgNailNm = fileSavePath +File.separator + "640"+DateUtil.getCurrDateTime() + fileExt;  // 중간에 점 제거
-			//String tmpFileNm = FileUtil.uploadTemp + originalFilename;
-			String tmpFileNm =  fileSavePath+File.separator +originalFilename;
-
-			log.info("파라머터: {}| {} | {} |  {}  " ,originalFilename, fileExt, imgNailNm,  tmpFileNm);
-
-			FileUtil.createDirectory(fileSavePath);
-			File savedFile = new File(tmpFileNm);
-			imgFile.transferTo(savedFile); // 업로드된 파일 저장
-			log.info("TEST {} | {} | {} | {}", paramModel.getDocStat(), paramModel.getDocFrom(),  paramModel.getDocTitle());
-
-	        // 썸네일 생성
-	        File thumbnailFile = new File(imgNailNm);
-	        Thumbnails.of(savedFile)
-	                  .size(700, 400)
-	                  .toFile(thumbnailFile);		
-			//file upload
-			
-			paramModel.setImgSrcNm(imgFile.getOriginalFilename());
-			paramModel.setImgNailNm(fileSavePath + DateUtil.getCurrDateTime() + fileExt);
-
-		} else {
-			log.info("imgFile is null ");
-			paramModel.setImgSrcNm(paramModel.getBefImgSrcNme());
-			paramModel.setImgNailNm(paramModel.getBefImgNailNme());
-		}
+		//파일 관련 내용 삭제
 		
 		log.info("EventUptProc , paramModel.getDocSeq() : " + paramModel.getDocSeq());
 		log.info("EventUptProc , paramModel.getDocTitle() : " + paramModel.getDocTitle());
